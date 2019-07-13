339245ef43f11f6a055f41ee4c7dce2cc837586c
Supan Adit Pratama <supanadit@gmail.com>
2019-07-13 14:38:27 +0700

    Update Readme

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,10 @@
 // lib/app.ts
 'use strict';
-import { ShellSecureModel } from "./model/ShellSecureModel";
-import { OperatingSystemOperation } from "./helper/OperatingSystemOperation";
+import { ShellSecureModel } from './model/ShellSecureModel';
+import { OperatingSystemOperation } from './helper/OperatingSystemOperation';
 
 import bodyParser = require('body-parser');
-import express = require("express");
+import express = require('express');
 
 const {Signale} = require('signale');
 const chalk = require('chalk');
@@ -24,33 +24,33 @@ app.post('/start', function (req, res) {
     try {
         ssh = req.body;
         const myOs: OperatingSystemOperation = new OperatingSystemOperation();
-        myOs.executeCommand("git clone https://supanadit@bitbucket.org/xenos500/beton-mobile-staff.git ./operation/beton");
+        myOs.executeCommand('git clone https://supanadit@bitbucket.org/xenos500/beton-mobile-staff.git ./operation/beton');
         res.send('Start Process for Host : ' + ssh.host);
     } catch (e) {
-        console.log("Error : " + e);
+        console.log('Error : ' + e);
     }
 });
 
-CFonts.say('Auto Engine', {
+CFonts.say('Operation X Engine', {
     font: 'block',
 });
 
-console.log("Software :", chalk.rgb(255, 255, 255).dim("Auto Engine"));
-console.log("");
-console.log("Author : Supan Adit Pratama", chalk.rgb(255, 255, 255).underline("<supan.aditp@xenos.co.id>"));
-console.log("");
-console.log("Version :", chalk.hex("#FFFFFF").bgBlue(" 1.0 "));
-console.log("");
-console.log("Current OS : ", chalk.hex("#FFFFFF").bgRed(" " + OperatingSystemOperation.checkOperatingSystem().name + " "));
-console.log("");
+console.log('Software :', chalk.rgb(255, 255, 255).dim('Auto Engine'));
+console.log('');
+console.log('Author : Supan Adit Pratama', chalk.rgb(255, 255, 255).underline('<supan.aditp@xenos.co.id>'));
+console.log('');
+console.log('Version :', chalk.hex('#FFFFFF').bgBlue(' 1.0 '));
+console.log('');
+console.log('Current OS : ', chalk.hex('#FFFFFF').bgRed(' ' + OperatingSystemOperation.checkOperatingSystem().name + ' '));
+console.log('');
 const processStart = new Signale({interactive: true, scope: 'Starting Server'});
-processStart.await("Starting Server");
+processStart.await('Starting Server');
 setTimeout(function () {
     if (OperatingSystemOperation.checkIsAllowedOperatingSystem()) {
         app.listen(3000, function () {
-            processStart.success("Engine success started on port 3000")
+            processStart.success('Engine success started on port 3000')
         });
     } else {
-        processStart.error("Engine failed to start");
+        processStart.error('Engine failed to start');
     }
 }, 1000);
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Operation X Engine
+
+![Terminal](https://i.ibb.co/9YYGJts/Operation-X-Engine.png)
+
+## Description
+
+This is main engine for any request in Operation X
+
+## Supported OS
+- Mac OS
+- Windows
+- Linux
+
+## This App Will Be Available For
+- Self Hosting
+- Official Hosting
+
+## Available
+- Client Only Mode
+- Client / Server Mode
+- Target Mode ( Soon ) will be available using two method ( Shell Script / Target App ( Golang ))
+
+## Todo
+- Support FTP / SFTP / SSH
+- VCS Support for SSH Method
+- Compressing Support for FTP / SFTP / SSH ( Upload Method )
+- Building App by Supported Technology
+- Deploy to selected server using ( Upload Method )
+- Realtime Log ( RethinkDB )
+- Terminal Operation ( SSH )
+- Auto Building and Deploy to selected server using ( Target Mode )
+- Create Plugin for Visual Studio Code, PHP Storm, Intellij Idea, Webstorm, Ruby Mine, Rider, Goland, Android Studio, Pycharm, Clion and Sublime Text
